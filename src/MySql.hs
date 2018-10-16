d01689934670f938693153200632df11f80b8b01
Veronika Romashkina <vrom911@gmail.com>
2018-10-16 16:45:14 +0800

    [#13] Add QQ (#28)
    * [#13] Add QQ
    
    * Fix after review

--- src/MySql.hs
+++ src/MySql.hs
@@ -1,7 +1,9 @@
 module MySql
        ( module MySql.Field
        , module MySql.Row
+       , module MySql.QQ
        ) where
 
 import MySql.Field
+import MySql.QQ
 import MySql.Row
---
+++ src/MySql/QQ.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | The 'sql' quasiquoter, for writing large @SQL@ statements.
+
+module MySql.QQ
+       ( sql
+       ) where
+
+import Data.String (fromString)
+import Database.MySQL.Base (Query)
+import Language.Haskell.TH (Exp, Q, appE, stringE)
+import Language.Haskell.TH.Quote (QuasiQuoter (..))
+
+{- | A quasiquoter for writing big @SQL@ queries.
+One should consider turning on the @-XQuasiQuotes@ pragma in that module:
+
+@
+{-# LANGUAGE QuasiQuoter #-}
+
+myQuery = query conn [sql|
+    SELECT
+      *
+    FROM
+      users
+    WHERE jobTitle = ?
+    |] jobTitle
+@
+-}
+sql :: QuasiQuoter
+sql = QuasiQuoter
+    { quotePat  = error "MySql.QQ.sql: quasiquoter used in pattern context"
+    , quoteType = error "MySql.QQ.sql: quasiquoter used in type context"
+    , quoteDec  = error "MySql.QQ.sql: quasiquoter used in declaration context"
+    , quoteExp  = sqlExp
+    }
+
+sqlExp :: String -> Q Exp
+sqlExp = appE [| fromString :: String -> Query |] . stringE
