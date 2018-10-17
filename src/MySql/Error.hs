ba9c45a208aff88c4c460f6375b6c0fef40b929e
Veronika Romashkina <vrom911@gmail.com>
2018-10-17 14:09:03 +0800

    [#31] Pretty Show instance for MySqlError (#37)
    * [#31] Pretty Show instance for MySqlError
    
    * Prettify show

--- src/MySql/Error.hs
+++ src/MySql/Error.hs
@@ -2,6 +2,11 @@ module MySql.Error
        ( MySqlError (..)
        ) where
 
+import Prelude hiding (show, unlines)
+
+import Data.List (unlines)
+import Text.Show (show)
+
 import qualified Database.MySQL.Base as SQL
 
 
@@ -10,4 +15,17 @@ data MySqlError
     = MySqlWrongField SQL.MySQLValue Text
     | MySqlExpectedEndOfRow (NonEmpty SQL.MySQLValue)
     | MySqlUnexpectedEndOfRow
-    deriving (Show, Eq)
+    deriving (Eq)
+
+instance Show MySqlError where
+    show = \case
+        MySqlWrongField val expected -> unlines
+            [ "MySQL error: Wrong field"
+            , "  Expected: " ++ show expected
+            , "  Actual: " ++ show val
+            ]
+        MySqlExpectedEndOfRow vals -> unlines
+            [ "MySql error: Expected end of rows"
+            , "  Remaining fields: " ++ show (toList vals)
+            ]
+        MySqlUnexpectedEndOfRow -> "MySql error: Unexpected end of row"
