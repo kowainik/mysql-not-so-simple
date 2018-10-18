f0214a99a0a296a9873d042a9d234a40a39158a1
Dmitry Kovanikov <kovanikov@gmail.com>
2018-10-18 18:34:36 +0800

    WIP TESTS DON'T LOOK!

--- sql/test.sql
+++ sql/test.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`
     ( `id`       INT(11) AUTO_INCREMENT PRIMARY KEY
     , `name`     TEXT      NOT NULL
-    , `birthday` TIMESTAMP NOT NULL
+--    , `birthday` TIMESTAMP NOT NULL
     , `weight`   DOUBLE    NOT NULL
     , `age`      INT(11)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main (main) where
 
 import Database.MySQL.Base (ciDatabase, ciPassword, ciUser, connect, defaultConnectInfo)
 import Hedgehog (Group, checkParallel)
 import System.IO (hSetEncoding, utf8)
 
-import MySql (executeRaw_)
+import MySql (MySqlError, Only, executeRaw, queryRaw, sql)
 import Test.Field (toField'fromField)
 import Test.Sql (insertSelect)
+
+import qualified Database.MySQL.Base as SQL
+import qualified System.IO.Streams as Streams
 
 -- fucking hedgehog can't into nested groups :(
 checkGroups :: [Group] -> IO ()
@@ -18,17 +23,51 @@ main = do
     hSetEncoding stdout utf8
     hSetEncoding stderr utf8
 
+    putTextLn "===> Establishing connection..."
     conn <- connect defaultConnectInfo
         { ciUser = "root"
         , ciPassword = "password"
         , ciDatabase = "test_db"
         }
     varConn <- newMVar conn
+    putTextLn "===> Connection established..."
 
     schema <- readFile "sql/test.sql"
-    executeRaw_ conn (fromString schema)
+    print =<< executeRaw conn (fromString schema)
+    putTextLn "===> Test schema created..."
 
-    checkGroups
-        [ toField'fromField
-        , insertSelect varConn
+    print =<< SQL.executeMany conn
+      "INSERT INTO `users` (`name`, `weight`, `age`) VALUES (?,?,?)"
+      [ [ SQL.MySQLText "Dima"
+        , SQL.MySQLDouble 80.3
+        , SQL.MySQLInt32 100
         ]
+      , [ SQL.MySQLText "Ivan"
+        , SQL.MySQLDouble 60
+        , SQL.MySQLInt32 20
+        ]
+
+      ]
+--    print =<< executeRaw conn [sql|
+--        INSERT INTO `users` (`name`, `birthday`, `weight`, `age`)
+--        VALUES ('Dima', '2018-10-18 15:49', 75.9, 100)
+--    |]
+    putTextLn "===> Test data inserted..."
+
+--    res :: Either MySqlError [Only Int32] <-
+--        runExceptT $ queryRaw conn [sql| SELECT LAST_INSERT_ID(); |]
+--    (defs, is) <- SQL.query_ conn "SELECT LAST_INSERT_ID();"
+    (defs, is) <- SQL.query_ conn "SELECT * FROM users"
+    putTextLn "===> ID queried..."
+    print =<< Streams.toList is
+
+    (defs, is) <- SQL.query_ conn "SELECT * FROM users"
+    putTextLn "===> ID queried..."
+    print =<< Streams.toList is
+
+    putTextLn "Tests finished"
+
+--     checkGroups
+--         [ toField'fromField
+--         , insertSelect varConn
+--         ]
