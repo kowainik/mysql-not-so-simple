996ad07f69b27534861520e97606e3764e724353
Dmitry Kovanikov <kovanikov@gmail.com>
2018-10-18 14:38:14 +0800

    [#32] Output column position in error

--- src/MySql/Matcher.hs
+++ src/MySql/Matcher.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE RecordWildCards #-}
+
 {- | Implements monad for writing 'FromRow' instances in a more convenient way.
 -}
 
 module MySql.Matcher
        ( Matcher (..)
+       , MatcherState (..)
        , Row
+
+       , mkMatcherState
        , usingMatcher
        , field
        ) where
@@ -18,6 +23,14 @@ import qualified Database.MySQL.Base as SQL
 
 type Row = [SQL.MySQLValue]
 
+data MatcherState = MatcherState
+    { matcherStatePos :: Int
+    , matcherStateRow :: Row
+    }
+
+mkMatcherState :: Row -> MatcherState
+mkMatcherState = MatcherState 0
+
 {- | Monad for parsing tables rows into custom data types.
 
 @
@@ -26,21 +39,23 @@ State Row (Either MySqlError) a
 @
 -}
 newtype Matcher a = Matcher
-    { runMatcher :: StateT Row (Either MySqlError) a
-    } deriving (Functor, Applicative, Monad, MonadState Row, MonadError MySqlError)
+    { runMatcher :: StateT MatcherState (Either MySqlError) a
+    } deriving (Functor, Applicative, Monad, MonadState MatcherState, MonadError MySqlError)
 
 -- | Runs matcher over given row and returns either error or result.
-usingMatcher :: Row -> Matcher a -> Either MySqlError a
-usingMatcher row matcher = do  -- using do-notation for Either monad
-    (a, remainFields) <- usingStateT row $ runMatcher matcher
-    case remainFields of
+usingMatcher :: MatcherState -> Matcher a -> Either MySqlError a
+usingMatcher matcherState matcher = do  -- using do-notation for Either monad
+    (a, finalState) <- usingStateT matcherState $ runMatcher matcher
+    case matcherStateRow finalState of
         []   -> Right a
         x:xs -> Left $ MySqlExpectedEndOfRow (x :| xs)
 
--- | Parser for single field.
+  -- | Parser for single field.
 field :: FromField a => Matcher a
-field = get >>= \case
-    [] -> throwError MySqlUnexpectedEndOfRow
-    val:vals -> case fromField val of
-        Left e  -> throwError e
-        Right x -> put vals >> pure x
+field = do
+    MatcherState{..} <- get
+    case matcherStateRow of
+        [] -> throwError MySqlUnexpectedEndOfRow
+        val:vals -> case fromField val of
+            Left e  -> throwError $ MySqlWrongColumn matcherStatePos e
+            Right x -> x <$ put (MatcherState (matcherStatePos + 1) vals)
