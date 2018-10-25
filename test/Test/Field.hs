bb55d0f86587a96b7b4945f4fc30a511b988088c
vrom911 <vrom911@gmail.com>
2018-10-25 12:25:21 +0800

    [#40] Add property tests

--- test/Test/Field.hs
+++ test/Test/Field.hs
@@ -7,18 +7,22 @@ module Test.Field
 import Hedgehog (Gen, Group (..), Property, forAll, property, tripping)
 
 import MySql (Field, FromField (..), MySqlError (..), ToField (..))
-import Test.Gen (genDouble, genInt32, genMaybe, genText, genUtcTime, named)
+import Test.Gen (genByteString, genDouble, genInt32, genLByteString, genLText, genMaybe, genText,
+                 genUtcTime, named)
 
 import qualified Database.MySQL.Base as SQL
 
 
 toField'fromField :: Group
 toField'fromField = Group "ToField/FromField"
-    [ fieldProperty genInt32   `named` "Int32"
-    , fieldProperty genDouble  `named` "Double"
-    , fieldProperty genText    `named` "Text"
-    , fieldProperty genMaybe   `named` "Maybe Int32"
-    , fieldProperty genUtcTime `named` "UtcTime"
+    [ fieldProperty genInt32       `named` "Int32"
+    , fieldProperty genDouble      `named` "Double"
+    , fieldProperty genText        `named` "Text"
+    , fieldProperty genLText       `named` "LText"
+    , fieldProperty genByteString  `named` "ByteString"
+    , fieldProperty genLByteString `named` "LByteString"
+    , fieldProperty genMaybe       `named` "Maybe Int32"
+    , fieldProperty genUtcTime     `named` "UtcTime"
     ]
 
 fieldProperty :: forall a . (Field a, Eq a, Show a) => Gen a -> Property
